c818ea4d9b6e37a7b779c34754a7abbaff8a7a30
mambax7 <mambax7@gmail.com>
2015-05-05 08:41:44 -0400

    Original files

---
+++ 1000ac_dark3/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "1000ac_dark3";
+	$txc_themeinfo["author"] = "1000-ads-canada";
+	$txc_themeinfo["author_url"] = "http://www.1000-ads-canada.com";
+	$txc_themeinfo["detail_url"] = "http://www.1000-ads-canada.com/xoops-themes/modules/mydownloads/";
+	$txc_themeinfo["download_url"] = "http://www.1000-ads-canada.com/xoops-themes/modules/mydownloads/";
+?>
